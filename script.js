c0432b69b975aaac9e35d193c33a97c7b412b56d
monkeybee11 <monkeybee1@googlemail.com>
2022-07-09 20:13:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 function squrt(target) {
 	
-	target.classList.add("wet");
+	target.classList.add("wet"); //kncoks the target over
 }
 
 function targetup(target) {
-	
-	target.classList.add("up");
-	
+
+	target.classList.add("up");//Uncaught TypeError: Cannot read properties of undefined (reading 'classList')
 	setTimeout( ()=> {
 		target.classList.remove("up");
 	}, 3000);
 }
 
-
-
+//see what targets arnt soaked
 function drytargets() {
 	return document.querySelector(".target:not(.wet)");
 }
 
 function randomtargetup() {
 	
-	var randomtarget = Math.random() * drytargets().length;
+	var randomtarget = Math.random() * drytargets().length; //randomly picks a dry taget
 	randomtarget = Math.floor(randomtarget);
 	var target = drytargets()[randomtarget];
 	
